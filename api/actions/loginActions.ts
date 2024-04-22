86c96a5bb2e90106263b41a67d69a109404f8bbd
Naveen <naveengummella.rg@gmail.com>
2024-04-23 01:31:34 +0530

    some constants

--- api/actions/loginActions.ts
+++ api/actions/loginActions.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from "axios";
 import { router } from "expo-router";
 import * as SecureStore from "expo-secure-store";
+import { callback, csrf, providers, session } from "../constants";
 
 export const getLogin = async () => {};
 
@@ -8,19 +9,20 @@ export const login = async (email: string, password: string) => {
   const apiUrl = process.env.EXPO_PUBLIC_API_URL;
   let credential_urls = "";
   let csrf_token = "";
+  console.log("")
   fetch(apiUrl + "/api/auth/signin", {
     method: "get",
   })
     .then((signin_response) => {
       axios
-        .get(apiUrl + "/api/auth/providers")
+        .get(apiUrl + providers)
         .then((provider_response) => {
           credential_urls = provider_response.data.credentials.signinUrl;
-          axios.get(apiUrl + "/api/auth/csrf").then((csrf_response) => {
+          axios.get(apiUrl + csrf).then((csrf_response) => {
             csrf_token = csrf_response.data.csrfToken;
             axios
-              .post(
-                apiUrl + "/api/auth/callback/credentials",
+              .post( 
+                apiUrl + callback,
                 {
                   username: email,
                   password,
@@ -32,7 +34,7 @@ export const login = async (email: string, password: string) => {
               )
               .then((credentials_response) => {
                 axios
-                  .get(apiUrl + "/api/auth/session")
+                  .get(apiUrl + session)
                   .then((session_response) => {
                     console.log(
                       "Session Details: " + session_response.data.user,
@@ -47,7 +49,7 @@ export const login = async (email: string, password: string) => {
                 console.log(error.request);
                 console.log("Error", error.status);
                 axios
-                  .get(apiUrl + "/api/auth/session")
+                  .get(apiUrl + session)
                   .then((session_response) => {
                     console.log(
                       "Session Details: " + session_response.data.user,
