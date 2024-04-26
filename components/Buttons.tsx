9e6737a09abfee4dac74b97400d11477f2735812
Naveen <naveengummella.rg@gmail.com>
2024-04-27 01:23:00 +0530

    fixing ref issue

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Pressable, StyleSheet } from "react-native";
 
 import { secondaryColor } from "@/constants/Colors";
-// Primary Button Coomponent to use the same Button everywhere possible So the UI remains the same
-export const PrimaryButton: React.FC<Button> = ({
-  children,
-  onPress,
-  disabled,
-}) => {
-  return (
-    <Pressable onPress={onPress} disabled={disabled} style={styles.container}>
-      {children}
-    </Pressable>
-  );
-};
+
+export interface ButtonProps {
+  children: React.ReactNode;
+  onPress: () => void;
+  disabled?: boolean;
+}
+
+export const PrimaryButton = React.forwardRef<React.ElementRef<typeof Pressable>, ButtonProps>(
+  ({ children, onPress, disabled }, ref) => {
+    return (
+      <Pressable ref={ref} onPress={onPress} disabled={disabled} style={styles.container}>
+        {children}
+      </Pressable>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
