a2847df32ca82b0c6d9cac568d1c079359259d6a
Naveen <naveengummella.rg@gmail.com>
2024-04-27 01:25:35 +0530

    another ref error resolution

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { PrimaryButton } from "@/components/Buttons";
-import { Text, View } from "@/components/Themed";
+import { Text, View } from "react-native";
 
 export default function AuthScreen() {
   return (
